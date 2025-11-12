6efbc18f932b7edee3cf47311b37b991bd21ed30
Rohan Kumar <crohank02@gmail.com>
2025-11-12 11:03:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import session from "express-session";
 import express from "express";
-import Hello from "./hello.js";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModulesRoutes from "./Kambaz/Modules/routes.js";

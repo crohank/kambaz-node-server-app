ca93318be33deb6c1f6c3a25aca30422e26cf605
Rohan Kumar <crohank02@gmail.com>
2025-12-01 20:39:28 -0500

    Update schema.js

--- Kambaz/Assignments/schema.js
+++ Kambaz/Assignments/schema.js
@@ -1,21 +1,3 @@
-// import mongoose from "mongoose";
-//
-// const assignmentSchema = new mongoose.Schema(
-//     {
-//         _id: String,
-//         title: String,
-//         course: String,
-//         releaseDate: Date,
-//         dueDate: Date,
-//         points: Number,
-//         modules: String,
-//         description: String,
-//     },
-//     { collection: "assignments" }
-// );
-//
-// export default assignmentSchema;
-
 import mongoose from "mongoose";
 
 const assignmentSchema = new mongoose.Schema(
